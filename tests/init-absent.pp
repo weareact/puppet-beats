442dccc6322571f2fc754f3a1f98ec6ba7483896
Patrik Majer <patrik.majer@atlas.cz>
2017-01-02 13:54:21 +0100

    add another manifest for testing

---
+++ tests/init-absent.pp
@@ -0,0 +1,29 @@
+# The baseline for module testing used by Puppet Labs is that each manifest
+# should have a corresponding test manifest that declares that class or defined
+# type.
+#
+# Tests are then run by using puppet apply --noop (to check for compilation
+# errors and view a log of events) or by fully applying the test in a virtual
+# environment (to compare the resulting system state to the desired state).
+#
+# Learn more about module testing here:
+# http://docs.puppetlabs.com/guides/tests_smoke.html
+#
+#include ::beats
+class { '::beats':
+  outputs_deep_merge => false,
+  outputs_logstash   => {
+    'filebeat' => { 'hosts' => [ 'logstash.example.com:5044' ], },
+    'topbeat'  => { 'hosts' => [ 'logstash.example.com:5044' ], },
+  },
+}
+
+#include ::beats::topbeat
+class { '::beats::topbeat':
+  ensure => 'absent',
+}
+
+#include ::beats::filebeat
+class { '::beats::filebeat':
+  ensure => 'absent',
+}
