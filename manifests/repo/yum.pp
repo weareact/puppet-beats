1f8e82396d597bb3b82d50bc8310485d91cc2bb1
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-03-17 10:44:00 +0100

    add suppport for elastic-5.x yum repo

--- manifests/repo/yum.pp
+++ manifests/repo/yum.pp
@@ -6,16 +6,30 @@ class beats::repo::yum {
     source => 'https://packages.elastic.co/GPG-KEY-elasticsearch',
   }
 
-  yumrepo { 'elastic-beats':
-    ensure   => 'present',
-    baseurl  => 'https://packages.elastic.co/beats/yum/el/$basearch',
-    descr    => 'Elastic Beats Repository',
-    enabled  => '1',
-    gpgcheck => '1',
-    gpgkey   => 'https://packages.elastic.co/GPG-KEY-elasticsearch',
-    require  => Rpmkey['D88E42B4'],
+  if ($::beats::use_repo_v5) {
+    yumrepo { 'elastic-beats':
+      ensure      => 'present',
+      baseurl     => 'https://artifacts.elastic.co/packages/5.x/yum',
+      descr       => 'Elastic repository for 5.x packages',
+      enabled     => '1',
+      gpgcheck    => '1',
+      gpgkey      => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+      require     => Rpmkey['D88E42B4'],
+    }
+  }
+  else
+  {
+    yumrepo { 'elastic-beats':
+      ensure   => 'present',
+      baseurl  => 'https://packages.elastic.co/beats/yum/el/$basearch',
+      descr    => 'Elastic Beats Repository',
+      enabled  => '1',
+      gpgcheck => '1',
+      gpgkey   => 'https://packages.elastic.co/GPG-KEY-elasticsearch',
+      require  => Rpmkey['D88E42B4'],
+    }
   }
 
-
 }
+
 
