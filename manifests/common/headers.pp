77524d96466d6cf5db18183f5c57ec5c5e82367a
Gavin Williams <fatmcgav@gmail.com>
2018-01-15 16:04:43 +0000

    Missed a 'version_v5' removal

--- manifests/common/headers.pp
+++ manifests/common/headers.pp
@@ -8,7 +8,6 @@ define beats::common::headers (
   $uid                   = $beats::uid,
   $gid                   = $beats::gid,
   $geoip_paths           = ['/usr/share/GeoIP/GeoIPCity.dat'],
-  $version_v5            = $beats::version_v5,
 ) {
   concat { "/etc/${title}/${title}.yml":
     ensure  => $ensure,
